00f809df985c16bfd4149044b8687cb1e5e71573
justinmorales1 <jm.em0210@gmail.com>
2021-03-11 20:32:39 -0600

    Adding husky

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,48 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 import GoogleAuth from "./GoogleAuth";
 
 class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <nav>
-                    <div className="nav-wrapper">
-                        <Link to='/' className="brand-logo" style={{marginLeft:'10px'}}> BWA - BasicWebApp </Link>
-                        <ul id="nav-mobile" className="right hide-on-med-and-down">
-                            <li><Link to='/employees-forms'>Employee Forms</Link></li>
-                            <li><Link to='/third-party-forms'>Third Party Forms</Link></li>
-                            <li><Link to='/member-forms'>Member Forms</Link></li>
-                            <li><Link to='/review-forms'>Review Forms</Link></li>
-                            <li>
-                                <GoogleAuth />
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <nav>
+          <div className="nav-wrapper">
+            <Link to="/" className="brand-logo" style={{ marginLeft: "10px" }}>
+              {" "}
+              Eat With Me{" "}
+            </Link>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <Link to="/employees-forms">Meat & Seafood</Link>
+              </li>
+              <li>
+                <Link to="/third-party-forms">Fruit & Vegetables</Link>
+              </li>
+              <li>
+                <Link to="/member-forms">Dairy & eggs</Link>
+              </li>
+              <li>
+                <Link to="/review-forms">Bakery</Link>
+              </li>
+              <li>
+                <Link to="/review-forms">Deli </Link>
+              </li>
+              <li>
+                <Link to="/review-forms">Pantry </Link>
+              </li>
+              <li>
+                <Link to="/review-forms">Beverages </Link>
+              </li>
+              <li>
+                <GoogleAuth />
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default Header;
