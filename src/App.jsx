05099f0da706f0be7dc6c1e0ea92303d8bf890b3
justinmorales1 <jm.em0210@gmail.com>
2021-03-11 20:37:33 -0600

    Adding routes for meat, seafood, veggies, etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,54 @@
-import React from 'react';
-import { Switch, BrowserRouter, Route } from 'react-router-dom';
-import Header from './components/Header';
-import LandingPage from './components/LandingPage'
-import MemberForms from './components/Tabs/MemberForms';
-import EmployeeForms from './components/Tabs/EmployeeForms';
-import ThirdPartyForms from './components/Tabs/ThirdPartyForms';
+import React from "react";
+import { Switch, BrowserRouter, Route, Link } from "react-router-dom";
+import Header from "./components/Header";
+import LandingPage from "./components/LandingPage";
+import MemberForms from "./components/Tabs/MemberForms";
+import EmployeeForms from "./components/Tabs/EmployeeForms";
+import ThirdPartyForms from "./components/Tabs/ThirdPartyForms";
 import ReviewForms from "./components/Tabs/ReviewForms";
 
-
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <BrowserRouter>
-                    <Header/>
-                    <Switch>
-                        <Route exact path='/' component={LandingPage}/>
-                        <Route exact path='/employees-forms' component={EmployeeForms}/>
-                        <Route exact path='/member-forms' component={MemberForms}/>
-                        <Route exact path='/third-party-forms' component={ThirdPartyForms}/>
-                        <Route exact path='/review-forms' component={ReviewForms}/>
-                    </Switch>
-                </BrowserRouter >
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/meat-and-seafood" component={EmployeeForms} />
+            <Route exact path="/fruit-and-vegetables" component={MemberForms} />
+            <Route exact path="/dairy-and-eggs" component={ThirdPartyForms} />
+            <Route exact path="/bakery" component={ReviewForms} />
+            <Route exact path="/deli" component={ReviewForms} />
+            <Route exact path="/pantry" component={ReviewForms} />
+            <Route exact path="/beverages" component={ReviewForms} />
+            <Route exact path="/account" component={ReviewForms} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// <li>
+//     <Link to="/meat-and-seafood">Meat & Seafood</Link>
+// </li>
+// <li>
+//     <Link to="/fruit-and-vegetables">Fruit & Vegetables</Link>
+// </li>
+// <li>
+//     <Link to="/dairy-and-eggs">Dairy & eggs</Link>
+// </li>
+// <li>
+//     <Link to="/bakery">Bakery</Link>
+// </li>
+// <li>
+//     <Link to="/deli">Deli </Link>
+// </li>
+// <li>
+//     <Link to="/pantry">Pantry </Link>
+// </li>
+// <li>
+//     <Link to="/beverages">Beverages </Link>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import GoogleAuth from "./GoogleAuth";
 
-
-
-
 class Header extends React.Component {
   render() {
     return (
@@ -17,29 +14,32 @@ class Header extends React.Component {
             </Link>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
               <li>
-                <Link to="/employees-forms">Meat & Seafood</Link>
+                <Link to="/meat-and-seafood">Meat & Seafood</Link>
               </li>
               <li>
-                <Link to="/third-party-forms">Fruit & Vegetables</Link>
+                <Link to="/fruit-and-vegetables">Fruit & Vegetables</Link>
               </li>
               <li>
-                <Link to="/member-forms">Dairy & eggs</Link>
+                <Link to="/dairy-and-eggs">Dairy & eggs</Link>
               </li>
               <li>
-                <Link to="/review-forms">Bakery</Link>
+                <Link to="/bakery">Bakery</Link>
               </li>
               <li>
-                <Link to="/review-forms">Deli </Link>
+                <Link to="/deli">Deli </Link>
               </li>
               <li>
-                <Link to="/review-forms">Pantry </Link>
+                <Link to="/pantry">Pantry </Link>
               </li>
               <li>
-                <Link to="/review-forms">Beverages </Link>
+                <Link to="/beverages">Beverages </Link>
               </li>
               <li>
-                <GoogleAuth />
+                <Link to="/account">Account </Link>
               </li>
+              {/*<li>*/}
+              {/*  <GoogleAuth />*/}
+              {/*</li>*/}
             </ul>
           </div>
         </nav>
