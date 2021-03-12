636c12e7871463c2f3ff8553ceb95504ab326c1f
justinmorales1 <jm.em0210@gmail.com>
2021-03-11 20:33:05 -0600

    Testing husky

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import GoogleAuth from "./GoogleAuth";
 
+
+
+
 class Header extends React.Component {
   render() {
     return (
