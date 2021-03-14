abd016ac6f9f8bc22ecd8577805cdfc928279c88
justinmorales1 <jm.em0210@gmail.com>
2021-03-14 18:25:08 -0500

    Adding card to handle grocerys

--- src/components/Grocery/MeatAndSeafood.jsx
+++ src/components/Grocery/MeatAndSeafood.jsx
@@ -1,12 +1,14 @@
 import { reduxForm } from "redux-form";
 import React from "react";
+import FoodCard from "./FoodCard";
+import Lobster from "../../assets/Lobster.png";
 
 class MeatAndSeafood extends React.Component {
   render() {
     return (
       <div className="container">
-        <div> Meat </div>
-        <div> Seafood </div>
+        <h1> Meat and Seafood </h1>
+        <FoodCard image={Lobster} />
       </div>
     );
   }
